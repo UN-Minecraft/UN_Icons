4011c14f9ab31ab594f9bc7666d24a2ebb88fecd
Carlos Sandoval <csandovalc@unal.edu.co>
2022-09-04 20:26:29 -0500

    Add: Manejo de equipos con luckperms

--- src/main/java/unmineraft/unicons/UNIcons.java
+++ src/main/java/unmineraft/unicons/UNIcons.java
@@ -4,47 +4,47 @@ import net.luckperms.api.LuckPerms;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
-import unmineraft.unicons.events.NewPlayerJoin;
+import unmineraft.unicons.teams.TeamsBuilder;
 import unmineraft.unicons.utilities.Utilities;
 
 public final class UNIcons extends JavaPlugin {
     PluginDescriptionFile pdfile = this.getDescription();
-
-    public LuckPerms api;
-
-    public String version = Utilities.translateColor("&a" + pdfile.getVersion());
+    public String version = Utilities.translateColor("&a" + this.pdfile.getVersion());
     public String name = Utilities.translateColor("&e[&aUNIcons&e]");
 
-    public void eventsRegister(){
-        PluginManager pluginManager = getServer().getPluginManager();
-        pluginManager.registerEvents(new NewPlayerJoin(this), this);
+    private LuckPerms api;
+
+    public LuckPerms getLuckPermsApi(){
+        return this.api;
     }
 
     @Override
     public void onEnable() {
-        String initPluginMessage = Utilities.translateColor(name + "&r&f has been enabled in the version: " + version);
+        String initPluginMessage = Utilities.translateColor(this.name + "&r&f has been enabled in the version: " + this.version);
         Bukkit.getConsoleSender().sendMessage(initPluginMessage);
 
         // Load LuckPerms api
         RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
         if (provider == null){
-            Bukkit.getConsoleSender().sendMessage(Utilities.translateColor("&l&cError: &rApi LuckPerms not load"));
+            Bukkit.getConsoleSender().sendMessage(this.name + Utilities.translateColor("&l&c Error: &rApi LuckPerms not load"));
         }
 
         if (provider != null){
             this.api = provider.getProvider();
         }
 
-        // Manage events
-        eventsRegister();
+        if (this.api == null) return;
+
+        String nameTemp = Utilities.translateColor("UNAL");
+        String prefixTemp = Utilities.translateColor("&l&4[UNAL]");
+        TeamsBuilder UN = new TeamsBuilder(this, nameTemp, prefixTemp);
     }
 
     @Override
     public void onDisable() {
-        String endPluginMessage = ChatColor.translateAlternateColorCodes('&', name + "&r&c has been disabled");
+        String endPluginMessage = ChatColor.translateAlternateColorCodes('&', this.name + "&r&c has been disabled");
         Bukkit.getConsoleSender().sendMessage(endPluginMessage);
     }
 }
